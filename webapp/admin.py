681172447e0b8167cdf90d433a4754a55d9936f8
Adam <adam.nemmish213@gmail.com>
2023-11-28 13:50:14 +0600

    Создал модель Task, зарегал в админке

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from webapp.models import Task
 # Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'status', 'date_finish']
+    list_filter = ['status']
+    search_fields = ['id', 'date_finish']
+    fields = ['description', 'status', 'date_finish']
+    readonly_fields = ['date_finish']
--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+   description = models.TextField(max_length=40, verbose_name='Описание')
+   status = models.CharField(max_length=50, verbose_name='Статус', default='new')
+   date_finish = models.DateField(verbose_name='Время выполнения', auto_now_add=True)
+
+   def __str__(self):
+      return f'{self.id}. {self.description}'
