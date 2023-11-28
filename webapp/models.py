48c3c7bdaf86ae44da74dc26010ea1ba3084aa08
Adam <adam.nemmish213@gmail.com>
2023-11-28 15:26:43 +0600

    Доработал модели и дополнил миграцию

--- webapp/models.py
+++ webapp/models.py
@@ -4,7 +4,8 @@ from django.db import models
 
 class Task(models.Model):
    description = models.TextField(max_length=40, verbose_name='Описание')
-   status = models.CharField(max_length=50, verbose_name='Статус', default='new')
+   status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'), ('done', 'Сделано')]
+   status = models.CharField(max_length=50, verbose_name='Статус', default='new', choices=status_choices)
    date_finish = models.DateField(verbose_name='Время выполнения', auto_now_add=True)
 
    def __str__(self):
