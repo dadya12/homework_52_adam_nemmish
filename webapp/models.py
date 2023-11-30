82be2fb1717fb7f9c9ca23179221aff71c036ce9
Adam <adam.nemmish213@gmail.com>
2023-12-01 00:21:29 +0600

    Добавил поле details_description в views.py, так добавил в models.py, затем в админке добавил новое поле, в шаблоне добавил ссылку подробнее..., а в шаблон по добавлению новой задачи добавил новое поле детальное описание, так же обновил фикстуры

--- webapp/models.py
+++ webapp/models.py
@@ -4,6 +4,7 @@ status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'),
 # Create your models here.
 class Task(models.Model):
    description = models.TextField(max_length=40, verbose_name='Описание')
+   details_description = models.TextField(max_length=50, verbose_name='Подробное описание', null=True, blank=True)
    status = models.CharField(max_length=50, verbose_name='Статус', default='new', choices=status_choices)
    date_finish = models.DateField(verbose_name='Время выполнения', null=True, blank=True)
 
